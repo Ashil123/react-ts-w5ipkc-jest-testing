aaa9218c02d115243562a7ab284450527566dd16
Ashil123 <45088949+Ashil123@users.noreply.github.com>
2022-11-29 10:43:48 +0530

    2 files modified, 2 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  const [val, setVal] = useState(0);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <p>{val}</p>
+      <button
+        onClick={() => {
+          setVal((prev) => prev + 1);
+        }}
+      >
+        Click me
+      </button>
     </div>
   );
 }
